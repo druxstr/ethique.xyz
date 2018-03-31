1bbcdb1037a8299d24d55e0af42989770bcc81e1
guillaume <git@whoz.me>
2018-03-31 12:00:46 +0200

    + matomo JS tracking

---
+++ static/js/custom.js
@@ -0,0 +1,3 @@
+// Matomo
+var _paq=_paq||[];_paq.push(["setDomains",["*.ethique.xyz"]]);_paq.push(["setDoNotTrack",true]);_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function(){var u="//whoz.me/all/piwik/";_paq.push(['setTrackerUrl',u+'piwik.php']);_paq.push(['setSiteId','4']);var d=document,g=d.createElement('script'),s=d.getElementsByTagName('script')[0];g.type='text/javascript';g.async=true;g.defer=true;g.src=u+'piwik.js';s.parentNode.insertBefore(g,s)})();
+
